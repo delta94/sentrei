eee1abffa42f7ba8196bacebae3c5ffb724e885c
Shun Kakinoki <shunkakinoki@gmail.com>
2020-07-27 20:12:17 +0900

    perf: ⚡️ update score actions

--- packages/functions/src/helpers/scoreActions.ts
+++ packages/functions/src/helpers/scoreActions.ts
@@ -1,9 +1,11 @@
 const scoreActions = {
-  created_spaces: 10,
+  created_invites: 3,
   created_members: 5,
   created_rooms: 3,
-  updated_spaces: 3,
+  created_spaces: 10,
   updated_rooms: 1,
+  updated_spaces: 3,
+  deleted_invites: 1,
   deleted_members: 3,
   deleted_rooms: 1,
 };
