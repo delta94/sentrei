1d9e416b028621dd4f4d4247fefbe4262f3b302b
Shun Kakinoki <shunkakinoki@gmail.com>
2020-07-30 22:56:52 +0900

    perf: ⚡️ refactor firebase admin w/ server runtime config

--- packages/common/src/utils/firebaseAdmin.ts
+++ packages/common/src/utils/firebaseAdmin.ts
@@ -1,10 +1,16 @@
 import * as admin from "firebase-admin";
+import getConfig from "next/config";
+
+const {serverRuntimeConfig} = getConfig();
 
 const firebaseAdminConfig = {
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/gm, "\n"),
+    clientEmail: serverRuntimeConfig.FIREBASE_CLIENT_EMAIL,
+    privateKey: serverRuntimeConfig.FIREBASE_PRIVATE_KEY?.replace(
+      /\\n/gm,
+      "\n",
+    ),
   }),
   databaseURL: process.env.FIREBASE_DATABASE_URL,
 };
