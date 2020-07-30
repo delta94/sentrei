b969780bb69e92ce962d1e90cf95de85f3d0a6ef
Shun Kakinoki <shunkakinoki@gmail.com>
2020-07-31 07:53:23 +0900

    perf: ⚡️ add room settings

--- packages/ui/src/components/RoomMenu/RoomMenu.tsx
+++ packages/ui/src/components/RoomMenu/RoomMenu.tsx
@@ -4,6 +4,7 @@ import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 import DeleteIcon from "@material-ui/icons/Delete";
 import EditAttributesIcon from "@material-ui/icons/EditAttributes";
+import SettingsIcon from "@material-ui/icons/Settings";
 import Link from "next-translate/Link";
 import useTranslation from "next-translate/useTranslation";
 import * as React from "react";
@@ -64,6 +65,17 @@ export default function RoomMenu({
             <DeleteIcon fontSize="small" />
           </ListItemIcon>
           <ListItemText primary={t("room:room.deleteRoom")} />
+        </MenuItem>
+      </Link>
+      <Link
+        href="/[spaceId]/room/[roomId]/settings"
+        as={`/${spaceId}/room/${roomId}/settings`}
+      >
+        <MenuItem>
+          <ListItemIcon>
+            <SettingsIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText primary={t("room:room.settings")} />
         </MenuItem>
       </Link>
     </Menu>
