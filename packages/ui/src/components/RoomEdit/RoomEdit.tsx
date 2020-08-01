eedb7619b20ab61be4a6de15917ce8430110e8de
Shun Kakinoki <shunkakinoki@gmail.com>
2020-08-01 20:53:08 +0900

    feat: 🎸 ini room name form

--- packages/ui/src/components/RoomEdit/RoomEdit.tsx
+++ packages/ui/src/components/RoomEdit/RoomEdit.tsx
@@ -1,3 +1,9 @@
+import Container from "@material-ui/core/Container";
+import AssignmentIndIcon from "@material-ui/icons/AssignmentInd";
+import LockIcon from "@material-ui/icons/Lock";
+import PhotoIcon from "@material-ui/icons/Photo";
+import SettingsIcon from "@material-ui/icons/Settings";
+import useTranslation from "next-translate/useTranslation";
 import Error from "next/error";
 import * as React from "react";
 
@@ -5,8 +11,11 @@ import {getRoom} from "@sentrei/common/firebase/rooms";
 import Profile from "@sentrei/types/models/Profile";
 import Room from "@sentrei/types/models/Room";
 import User from "@sentrei/types/models/User";
-import RoomForm from "@sentrei/ui/components/RoomForm";
+import FormSection from "@sentrei/ui/components/FormSection";
+import RoomNameForm from "@sentrei/ui/components/RoomNameForm";
+import SettingsUsernameForm from "@sentrei/ui/components/SettingsUsernameForm";
 import SkeletonForm from "@sentrei/ui/components/SkeletonForm";
+import TabBoard from "@sentrei/ui/components/TabBoard";
 
 export interface Props {
   profile: Profile.Get;
@@ -15,12 +24,8 @@ export interface Props {
   user: User.Get;
 }
 
-export default function RoomEdit({
-  profile,
-  roomId,
-  spaceId,
-  user,
-}: Props): JSX.Element {
+export default function RoomEdit({profile, roomId, user}: Props): JSX.Element {
+  const {t} = useTranslation();
   const [room, setRoom] = React.useState<Room.Get | null | undefined>();
 
   React.useEffect(() => {
@@ -36,12 +41,32 @@ export default function RoomEdit({
   }
 
   return (
-    <RoomForm
-      type="edit"
-      profile={profile}
-      room={room}
-      user={user}
-      spaceId={spaceId}
-    />
+    <FormSection
+      icon={<SettingsIcon />}
+      title={t("room:room.editRoom")}
+      size="md"
+    >
+      <>
+        <TabBoard
+          tabIconOne={<AssignmentIndIcon />}
+          tabIconTwo={<PhotoIcon />}
+          tabIconThree={<LockIcon />}
+          tabLabelOne={t("common:common.name")}
+          tabLabelTwo={t("common:common.photo")}
+          tabLabelThree={t("common:common.username")}
+          tabPanelOne={
+            <Container component="main" maxWidth="xs">
+              <RoomNameForm profile={profile} room={room} user={user} />
+            </Container>
+          }
+          tabPanelTwo={<></>}
+          tabPanelThree={
+            <Container component="main" maxWidth="xs">
+              <SettingsUsernameForm profile={profile} user={user} />
+            </Container>
+          }
+        />
+      </>
+    </FormSection>
   );
 }
