0803e9814b9d862327ea73b1490844d48acd759d
Shun Kakinoki <shunkakinoki@gmail.com>
2020-08-06 08:08:41 +0900

    feat: 🎸 space panel refactor (banner, status, panel)

--- packages/ui/src/components/SpacePanel/SpacePanel.tsx
+++ packages/ui/src/components/SpacePanel/SpacePanel.tsx
@@ -1,15 +1,10 @@
-import Avatar from "@material-ui/core/Avatar";
 import Box from "@material-ui/core/Box";
-import Container from "@material-ui/core/Container";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import DashboardIcon from "@material-ui/icons/Dashboard";
 
 import * as React from "react";
 
 import SpacePanelAccordion from "@sentrei/ui/components/SpacePanelAccordion";
-
-import SpacePanelStyles from "./SpacePanelStyles";
+import SpacePanelBanner from "@sentrei/ui/components/SpacePanelBanner";
+import SpacePanelStatus from "@sentrei/ui/components/SpacePanelStatus";
 
 export interface Props {
   photo?: string | null;
@@ -18,24 +13,11 @@ export interface Props {
 }
 
 export default function SpacePanel({photo, name, spaceId}: Props): JSX.Element {
-  const classes = SpacePanelStyles();
-
   return (
     <>
-      <Container maxWidth="xs">
-        <Grid container alignItems="center" justify="center" direction="row">
-          {photo ? (
-            <Avatar src={photo || undefined} className={classes.large} />
-          ) : (
-            <DashboardIcon color="disabled" className={classes.large} />
-          )}
-          <Box p={2}>
-            <Typography variant="h2" component="h2" align="center">
-              {name}
-            </Typography>
-          </Box>
-        </Grid>
-      </Container>
+      <SpacePanelBanner photo={photo} name={name} />
+      <SpacePanelStatus />
+      <Box py={2} />
       <SpacePanelAccordion spaceId={spaceId} />
     </>
   );
---
+++ packages/ui/src/components/SpacePanelStatus/SpacePanelStatus.tsx
@@ -0,0 +1,59 @@
+import ButtonBase from "@material-ui/core/ButtonBase";
+import Container from "@material-ui/core/Container";
+import Divider from "@material-ui/core/Divider";
+import Grid from "@material-ui/core/Grid";
+import IconButton from "@material-ui/core/IconButton";
+import InputBase from "@material-ui/core/InputBase";
+import Paper from "@material-ui/core/Paper";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import KeyboardReturnIcon from "@material-ui/icons/KeyboardReturn";
+import {Emoji} from "emoji-mart";
+import * as React from "react";
+
+import SpacePanelStatusStyles from "./SpacePanelStatusStyles";
+
+export interface Props {
+  photo?: string | null;
+  name: string;
+  spaceId: string;
+}
+
+export default function SpacePanelStatus(): JSX.Element {
+  const classes = SpacePanelStatusStyles();
+
+  return (
+    <Container maxWidth="md">
+      <Grid container alignItems="center" justify="center" spacing={3}>
+        <Grid item xs={12} sm={1} md={2} />
+        <Grid item xs={12} sm={10} md={8}>
+          <Paper component="form" className={classes.root}>
+            <ButtonBase className={classes.iconButton} aria-label="emoji">
+              <Emoji emoji="santa" set="twitter" size={30} />
+            </ButtonBase>
+            <Divider className={classes.divider} orientation="vertical" />
+            <InputBase
+              className={classes.input}
+              placeholder="Write your status"
+              inputProps={{"aria-label": "search google maps"}}
+            />
+            <IconButton
+              type="submit"
+              className={classes.iconButton}
+              aria-label="search"
+            >
+              <KeyboardReturnIcon />
+            </IconButton>
+            <IconButton
+              color="primary"
+              className={classes.iconButton}
+              aria-label="directions"
+            >
+              <GitHubIcon />
+            </IconButton>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={1} md={2} />
+      </Grid>
+    </Container>
+  );
+}
