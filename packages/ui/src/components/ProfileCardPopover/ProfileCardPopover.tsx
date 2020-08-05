562bf57edaeb466da4439ca01083a052f069e1df
Shun Kakinoki <shunkakinoki@gmail.com>
2020-08-05 15:27:31 +0900

    feat: 🎸 ini profile popover

---
+++ packages/ui/src/components/ProfileCardPopover/ProfileCardPopover.tsx
@@ -0,0 +1,47 @@
+import Popover from "@material-ui/core/Popover";
+import Typography from "@material-ui/core/Typography";
+import * as React from "react";
+
+import Member from "@sentrei/types/models/Member";
+
+import ProfileCardPopoverStyles from "./ProfileCardPopoverStyles";
+
+export interface Props {
+  anchorEl: HTMLElement | null;
+  member: Member.Get;
+  open: boolean;
+  onClose: () => void;
+}
+
+export default function ProfileCardPopover({
+  anchorEl,
+  member,
+  open,
+  onClose,
+}: Props): JSX.Element {
+  const classes = ProfileCardPopoverStyles();
+
+  return (
+    <Popover
+      id="mouse-over-popover"
+      className={classes.popover}
+      classes={{
+        paper: classes.paper,
+      }}
+      open={open}
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "left",
+      }}
+      transformOrigin={{
+        vertical: "bottom",
+        horizontal: "left",
+      }}
+      onClose={onClose}
+      disableRestoreFocus
+    >
+      <Typography>{member.name}</Typography>
+    </Popover>
+  );
+}
