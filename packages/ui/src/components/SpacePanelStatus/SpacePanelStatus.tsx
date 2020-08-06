401ca074ab4e6170e94ad32ba22b52fe30805820
Shun Kakinoki <shunkakinoki@gmail.com>
2020-08-06 14:44:49 +0900

    feat: 🎸 ini space panel description

--- packages/ui/src/components/SpacePanelStatus/SpacePanelStatus.tsx
+++ packages/ui/src/components/SpacePanelStatus/SpacePanelStatus.tsx
@@ -2,15 +2,14 @@ import Container from "@material-ui/core/Container";
 import Divider from "@material-ui/core/Divider";
 import Grid from "@material-ui/core/Grid";
 import IconButton from "@material-ui/core/IconButton";
-import InputBase from "@material-ui/core/InputBase";
 import Paper from "@material-ui/core/Paper";
 import GitHubIcon from "@material-ui/icons/GitHub";
-import KeyboardReturnIcon from "@material-ui/icons/KeyboardReturn";
 import * as React from "react";
 
 import Member from "@sentrei/types/models/Member";
 import Profile from "@sentrei/types/models/Profile";
 import User from "@sentrei/types/models/User";
+import SpacePanelDescriptionForm from "@sentrei/ui/components/SpacePanelDescriptionForm";
 import SpacePanelEmojiPicker from "@sentrei/ui/components/SpacePanelEmojiPicker";
 
 import SpacePanelStatusStyles from "./SpacePanelStatusStyles";
@@ -43,18 +42,12 @@ export default function SpacePanelStatus({
               userId={user.uid}
             />
             <Divider className={classes.divider} orientation="vertical" />
-            <InputBase
-              className={classes.input}
-              placeholder="Write your status"
-              inputProps={{"aria-label": "write your emoji"}}
+            <SpacePanelDescriptionForm
+              profile={profile}
+              member={member}
+              spaceId={spaceId}
+              userId={user.uid}
             />
-            <IconButton
-              type="submit"
-              className={classes.iconButton}
-              aria-label="search"
-            >
-              <KeyboardReturnIcon />
-            </IconButton>
             <IconButton
               color="primary"
               className={classes.iconButton}
